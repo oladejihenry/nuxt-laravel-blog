41e31d3cf0e59336d485bd1cbdb1c69f2623bdc2
Henry Oladeji <henryoladeji2007@gmail.com>
2022-03-14 19:33:47 +0000

    added post crud

--- app/Http/Controllers/PostController.php
+++ app/Http/Controllers/PostController.php
@@ -7,7 +7,12 @@ use Illuminate\Http\Request;
 use App\Models\Post;
 
 class PostController extends Controller
-{
+{   
+    /**
+    *   Display all post in the order of created_at.
+    *   And also in desc order then paginate.
+    *   @return \Illuminate\Http\Response
+    */
     public function index()
     {
         $posts = Post::orderBy('created_at', 'desc')->paginate(10);
@@ -15,6 +20,12 @@ class PostController extends Controller
         return response()->json($posts);
     }
 
+    /**
+     * Stores new post.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
     public function store(PostRequest $request)
     {  
         $post = Post::create([
