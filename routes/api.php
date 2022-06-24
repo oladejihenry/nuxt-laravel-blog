47df31c0487b28cc06a5c3855476641767f765ac
Henry Oladeji <henryoladeji2007@gmail.com>
2022-06-24 16:10:20 +0100

    Api Update

--- routes/api.php
+++ routes/api.php
@@ -17,9 +17,9 @@ use App\Http\Controllers\{UserController, PostController, CategoryController, Fr
 |
 */
 
-// Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
-//     return $request->user();
-// });
+Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
+    return $request->user();
+});
 
 
 //Auth
