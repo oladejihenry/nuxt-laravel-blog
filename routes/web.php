17b05f271ae22600deb29bf830d581f6f94c359a
Henry Oladeji <henryoladeji2007@gmail.com>
2022-06-24 21:10:17 +0100

    Minor Update

--- routes/web.php
+++ routes/web.php
@@ -2,6 +2,7 @@
 
 use Illuminate\Support\Facades\Route;
 
+
 /*
 |--------------------------------------------------------------------------
 | Web Routes
@@ -16,6 +17,8 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
+
 
 // Route::get('/login', function () {
 //     return 'hi';
