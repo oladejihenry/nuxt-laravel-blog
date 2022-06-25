9a68a1732b125fbeeebd842426e492ee1081bb54
Henry Oladeji <henryoladeji2007@gmail.com>
2022-06-25 02:55:16 +0100

    Minor Update

--- config/cors.php
+++ config/cors.php
@@ -16,7 +16,7 @@ return [
     */
 
     // 'paths' => ['*'],
-    'paths' => ['api/*', 'sanctum/csrf-cookie', '/login', '/logout'],
+    'paths' => ['api/*', 'sanctum/csrf-cookie', '/login', '/logout', '/register'],
 
     'allowed_methods' => ['*'],
 
